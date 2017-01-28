d35d06c49a90d086370889b798f993d668da41df
Administrator <Administrator@JMWO3GAR1P7TMIT>
2017-01-28 22:16:02 +0800

    新增电流对象

---
+++ src/com/elefirst/powerdetail/po/CurrentDetail.java
@@ -0,0 +1,48 @@
+package com.elefirst.powerdetail.po;
+
+public class CurrentDetail {
+	private String maxACurrent;
+	private String maxBCurrent;
+	private String maxCCurrent;
+	
+	private String minACurrent;
+	private String minBCurrent;
+	private String minCCurrent;
+	public String getMaxACurrent() {
+		return maxACurrent;
+	}
+	public void setMaxACurrent(String maxACurrent) {
+		this.maxACurrent = maxACurrent;
+	}
+	public String getMaxBCurrent() {
+		return maxBCurrent;
+	}
+	public void setMaxBCurrent(String maxBCurrent) {
+		this.maxBCurrent = maxBCurrent;
+	}
+	public String getMaxCCurrent() {
+		return maxCCurrent;
+	}
+	public void setMaxCCurrent(String maxCCurrent) {
+		this.maxCCurrent = maxCCurrent;
+	}
+	public String getMinACurrent() {
+		return minACurrent;
+	}
+	public void setMinACurrent(String minACurrent) {
+		this.minACurrent = minACurrent;
+	}
+	public String getMinBCurrent() {
+		return minBCurrent;
+	}
+	public void setMinBCurrent(String minBCurrent) {
+		this.minBCurrent = minBCurrent;
+	}
+	public String getMinCCurrent() {
+		return minCCurrent;
+	}
+	public void setMinCCurrent(String minCCurrent) {
+		this.minCCurrent = minCCurrent;
+	}
+	
+}
