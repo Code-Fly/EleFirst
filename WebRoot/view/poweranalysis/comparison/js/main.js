36fe8747daa5bdc006218cd35a322b5c60508ff7
Barrie <barrieshieh@gmail.com>
2017-01-31 03:51:50 +0800

    添加对比分析时表格动态添加所有监测点

--- WebRoot/view/poweranalysis/comparison/js/main.js
+++ WebRoot/view/poweranalysis/comparison/js/main.js
@@ -115,6 +115,34 @@ $(document).ready(function () {
         editable: false,
     });
 
+    $("#tab-table").tabs({
+        onAdd: function (title, index) {
+            var panel = $(this).tabs("getTab", index);
+            var table = $(panel).find(".dg-table");
+            $(table).datagrid({
+                // data: d,
+                fitColumns: true,
+                border: false,
+                singleSelect: true,
+                columns: [[
+                    {
+                        field: "clientOperationTime",
+                        title: "日期",
+                        align: "center",
+                        width: 100
+                    },
+                    {field: "totalactivepower", title: "最大值", align: "center", width: 100},
+                    {
+                        field: "currentClientOperationTime",
+                        title: "发生时间",
+                        align: "center",
+                        width: 100
+                    }
+                ]]
+            });
+        }
+    });
+
     $("#btn-search").linkbutton({
         onClick: function () {
             var node = $("#input-pn").tagbox("getData");
@@ -185,76 +213,84 @@ $(document).ready(function () {
                 }
             });
 
-            var paramTable = {
-                node: [],
-                time: []
-            }
-
-            var areaId = (node[0].value + "").split(":")[0];
-            var concentratorId = (node[0].value + "").split(":")[1];
-            var pn = (node[0].value + "").split(":")[2];
-            var pt = parseFloat((node[0].value + "").split(":")[3]);
-            var ct = parseFloat((node[0].value + "").split(":")[4]);
-            var name = node[0].name;
-            paramTable.node.push({
-                areaId: areaId,
-                concentratorId: concentratorId,
-                pn: pn
-            })
-            for (var i = 0; i < time.length; i++) {
-                var ss = time[i].value.split('-');
-                var y = parseInt(ss[0], 10);
-                var m = parseInt(ss[1], 10) - 1;
-                var d = parseInt(ss[2], 10);
-
-                paramTable.time.push(
-                    new Date(y, m, d).format('yyyyMMdd') + "000000"
-                )
-            }
-
-            $("#label-pn-table").text(name);
-
-            $.ajax({
-                url: _ctx + "poweranalysis/comparison/load/daily/table.do",
-                type: "POST",
-                cache: false,
-                contentType: "text/plain;charset=UTF-8",
-                data: JSON.stringify(paramTable),
-                success: function (r) {
-                    if (r.hasOwnProperty("errcode")) {
-                        if ("0" == r.errcode) {
-                            // $.messager.alert("操作提示", JSON.stringify(r.data));
-                            var d = [];
-                            for (var i = 0; i < r.data.length; i++) {
-                                var time = r.data[i].clientoperationtime + "";
-                                var totalactivepower = parseFloat(r.data[i].maxTotalActivePower) * pt * ct;
-                                totalactivepower = Math.floor(totalactivepower * 100) / 100;
-                                d.push({
-                                    clientOperationTime: (formatDbTimestamp(time) + "").substr(0, 10),
-                                    totalactivepower: totalactivepower,
-                                    currentClientOperationTime: formatDbTimestamp(time)
-                                })
-                            }
-                            $("#dg-table").datagrid("loadData", d);
-                            // getLoadChart(r.data);
-                        } else {
-                            $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg(r.errcode), "info");
-                        }
-                    } else {
-                        $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg("2"), "info");
-                    }
-                },
-                beforeSend: function (XMLHttpRequest) {
-                    MaskUtil.mask();
-                },
-                error: function (request) {
-                    $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg("3"), "info");
-                },
-                complete: function (XMLHttpRequest, textStatus) {
-                    MaskUtil.unmask();
+            for (var i = 0; i < node.length; i++) {
+                var paramTable = {
+                    node: [],
+                    time: []
                 }
-            });
 
+                var areaId = (node[i].value + "").split(":")[0];
+                var concentratorId = (node[i].value + "").split(":")[1];
+                var pn = (node[i].value + "").split(":")[2];
+                var pt = parseFloat((node[i].value + "").split(":")[3]);
+                var ct = parseFloat((node[i].value + "").split(":")[4]);
+                var name = node[i].name;
+                paramTable.node.push({
+                    areaId: areaId,
+                    concentratorId: concentratorId,
+                    pn: pn
+                });
+
+                for (var j = 0; j < time.length; j++) {
+                    var ss = time[j].value.split('-');
+                    var y = parseInt(ss[0], 10);
+                    var m = parseInt(ss[1], 10) - 1;
+                    var d = parseInt(ss[2], 10);
+
+                    paramTable.time.push(
+                        new Date(y, m, d).format('yyyyMMdd') + "000000"
+                    )
+                }
+
+                $.ajax({
+                    url: _ctx + "poweranalysis/comparison/load/daily/table.do",
+                    type: "POST",
+                    cache: false,
+                    contentType: "text/plain;charset=UTF-8",
+                    data: JSON.stringify(paramTable),
+                    success: function (r, textStatus, jqXHR) {
+                        if (r.hasOwnProperty("errcode")) {
+                            if ("0" == r.errcode) {
+                                // $.messager.alert("操作提示", JSON.stringify(r.data));
+                                var d = [];
+                                for (var j = 0; j < r.data.length; j++) {
+                                    var time = r.data[j].clientoperationtime + "";
+                                    var totalactivepower = parseFloat(r.data[j].maxTotalActivePower) * pt * ct;
+                                    totalactivepower = Math.floor(totalactivepower * 100) / 100;
+                                    d.push({
+                                        clientOperationTime: (formatDbTimestamp(time) + "").substr(0, 10),
+                                        totalactivepower: totalactivepower,
+                                        currentClientOperationTime: formatDbTimestamp(time)
+                                    })
+                                }
+
+                                if (r.data.length > 0) {
+                                    $(".table-" + r.data[0].areaId + "-" + r.data[0].concentratorId + "-" + r.data[0].pn).datagrid("loadData", d);
+                                }
+                            } else {
+                                $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg(r.errcode), "info");
+                            }
+                        } else {
+                            $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg("2"), "info");
+                        }
+                    },
+                    beforeSend: function (XMLHttpRequest) {
+                        if (!$("#tab-table").tabs("getTab", name)) {
+                            $("#tab-table").tabs("add", {
+                                title: name,
+                                content: "<div class='dg-table table-" + areaId + "-" + concentratorId + "-" + pn + "'></div>"
+                            })
+                        }
+                        // MaskUtil.mask();
+                    },
+                    error: function (request) {
+                        $.messager.alert("操作提示", "请求失败！" + DsmErrUtils.getMsg("3"), "info");
+                    },
+                    complete: function (XMLHttpRequest, textStatus) {
+                        // MaskUtil.unmask();
+                    }
+                });
+            }
         }
     });
 
