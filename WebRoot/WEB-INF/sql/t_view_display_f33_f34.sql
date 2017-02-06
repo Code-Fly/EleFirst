13ed1b377d416c83006d5f2259580c3585691328
Administrator <Administrator@JMWO3GAR1P7TMIT>
2017-02-07 01:13:47 +0800

    视图sql脚本

--- WebRoot/WEB-INF/sql/t_view_display_f33_f34.sql
+++ WebRoot/WEB-INF/sql/t_view_display_f33_f34.sql
@@ -1,12 +1,1 @@
-CREATE VIEW t_view_display_f33_f34 AS SELECT
-  a.totalPositiveActivePower,a.totalPositiveReactivePower,a.id as id33,a.area_id as area_id33,a.clientOperationTime as clientOperationTime33,a.concentrator_id as concentrator_id33,a.pn as pn33,a.rate as rate33,a.sendTime as sendTime33,
-  b.totalReverseActivePower,b.totalReverseReactivePower,b.id as id34,b.area_id as area_id34,b.clientOperationTime as clientOperationTime34,b.concentrator_id as concentrator_id34,b.pn as pn34,b.rate as rate34,b.sendTime as sendTime34
-FROM
-	t_004_type_one_data_fn33 a,
-	t_006_type_one_data_fn34 b
-WHERE
-	a.concentrator_id = b.concentrator_id
-AND a.pn = b.pn
-AND a.area_id = b.area_id
-AND a.clientOperationTime = b.clientOperationTime and a.clientOperationTime is NOT null 
-AND b.clientOperationTime is not NULL
+select `a`.`totalPositiveActivePower` AS `totalPositiveActivePower`,`a`.`totalPositiveReactivePower` AS `totalPositiveReactivePower`,`a`.`id` AS `id33`,`a`.`area_id` AS `area_id33`,`a`.`clientOperationTime` AS `clientOperationTime33`,`a`.`concentrator_id` AS `concentrator_id33`,`a`.`pn` AS `pn33`,`a`.`rate` AS `rate33`,`a`.`sendTime` AS `sendTime33`,`b`.`totalReverseActivePower` AS `totalReverseActivePower`,`b`.`totalReverseReactivePower` AS `totalReverseReactivePower`,`b`.`id` AS `id34`,`b`.`area_id` AS `area_id34`,`b`.`clientOperationTime` AS `clientOperationTime34`,`b`.`concentrator_id` AS `concentrator_id34`,`b`.`pn` AS `pn34`,`b`.`rate` AS `rate34`,`b`.`sendTime` AS `sendTime34` from (`t_004_type_one_data_fn33` `a` join `t_006_type_one_data_fn34` `b`) where ((`a`.`concentrator_id` = `b`.`concentrator_id`) and (`a`.`pn` = `b`.`pn`) and (`a`.`area_id` = `b`.`area_id`) and (`a`.`clientOperationTime` = `b`.`clientOperationTime`) and (`a`.`clientOperationTime` is not null) and (`b`.`clientOperationTime` is not null))
