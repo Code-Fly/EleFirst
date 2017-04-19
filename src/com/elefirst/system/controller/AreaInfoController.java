67ce09dc04d5b5eec10a2b9761c4aef89689bd07
VM <vm@xxx.com>
2017-04-19 17:20:58 +0800

    asdasdasd

--- src/com/elefirst/system/controller/AreaInfoController.java
+++ src/com/elefirst/system/controller/AreaInfoController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * Created by     xxxxx barrie on 17/1/29.
+ * Created by    1111 xxxxx barrie on 17/1/29.
  */
 @Controller
 @RequestMapping("/system/area")
