9070292f967c17a1bcb8d6b876d6ba8d863aa05a
Barrie <barrieshieh@gmail.com>
2017-04-23 18:37:20 +0800

    fix

--- WebRoot/view/poweranalysis/load/js/monthly.js
+++ WebRoot/view/poweranalysis/load/js/monthly.js
@@ -136,17 +136,19 @@ $(document).ready(function () {
         var endDate = new Date(y, m);
         endDate.setMonth(endDate.getMonth() + interval + 1);
 
-        var startTime = startDate.format("yyyyMM") + "01000000";
-        var endTime = endDate.format("yyyyMM") + "01000000";
+        var timeList = [];
+        timeList.push({
+            startTime: startDate.format('yyyyMM') + "01000000",
+            endTime: endDate.format('yyyyMM') + "01000000"
+        });
 
         $.ajax({
-            url: _ctx + "power/data/f25/frozen/minute/node/sum.do",
+            url: _ctx + "power/data/f25f5/frozen/minute/node/time/sum.do",
             type: "POST",
             cache: false,
             data: {
                 node: JSON.stringify(pnList),
-                startTime: startTime,
-                endTime: endTime
+                time: JSON.stringify(timeList)
             },
             success: function (r) {
                 if (r.hasOwnProperty("errcode")) {
@@ -159,7 +161,7 @@ $(document).ready(function () {
 
                         var item = ChartUtils.getLoadAllSeries({
                             name: "最大"
-                        }, r.data);
+                        }, r.data[0]);
                         item.dataGrouping = {
                             approximation: "high",
                             forced: true
@@ -168,18 +170,19 @@ $(document).ready(function () {
 
                         var item = ChartUtils.getLoadAllSeries({
                             name: "最小"
-                        }, r.data);
+                        }, r.data[0]);
                         item.dataGrouping = {
                             approximation: "low",
                             forced: true
                         };
                         series.push(item);
 
-                        var item = ChartUtils.getLoadAllSeries({
+                        var item = ChartUtils.getF5AllSeries({
                             name: "平均"
-                        }, r.data);
+                        }, r.data[0]);
                         item.dataGrouping = {
-                            approximation: "average",
+                            valueDecimals: 3,
+                            approximation: ChartUtils.approximations.averageLoad,
                             forced: true
                         };
                         series.push(item);
