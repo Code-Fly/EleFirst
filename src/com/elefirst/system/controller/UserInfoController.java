df647c28b830e5beb520b2eb4227fb46157735ab
Administrator <dddd>
2017-05-23 18:55:11 +0800

    用户管理新增,删除,修改,刷新

--- src/com/elefirst/system/controller/UserInfoController.java
+++ src/com/elefirst/system/controller/UserInfoController.java
@@ -5,13 +5,17 @@ import com.elefirst.base.entity.DataGrid;
 import com.elefirst.base.entity.Error;
 import com.elefirst.base.entity.ErrorMsg;
 import com.elefirst.base.exception.SessionExpiredException;
+import com.elefirst.base.utils.UUIDKeyGenerator;
 import com.elefirst.system.po.UserInfo;
 import com.elefirst.system.po.UserInfoCustom;
 import com.elefirst.system.service.iface.IUserInfoService;
 import com.google.gson.Gson;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -134,6 +140,83 @@ public class UserInfoController extends BaseController {
         template.setCreateDate(new Date());
         int result = userInfoService.addUserInfo(template);
         return new ErrorMsg(Error.SUCCESS, "success", result);
+    }
+    
+    @RequestMapping("/info/updatePersonInfo.do")
+    @ResponseBody
+    public ErrorMsg updatePersonInfo(@RequestParam(value = "userId", required = false)String userId, 
+    		@RequestParam(value = "userCode", required = false)String userCode, 
+    		@RequestParam(value = "userName", required = false)String userName, 
+    		@RequestParam(value = "description", required = false)String description,  
+    		@RequestParam(value = "passwd", required = false)String passwd, 
+    		HttpServletRequest request, HttpSession session) {
+    	
+    	UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
+        if (null == userInfo || null == userInfo.getUserName()) {
+            logger.error("用户Session过期");
+            return new ErrorMsg(Error.SESSION_EXPIRED_EXCEPTION, "Session expired");
+        }
+        try {
+        	UserInfo personInfo = new UserInfo();
+        	personInfo.setId(userId);
+        	personInfo.setUserCode(userCode);
+        	personInfo.setUserName(userName);
+            personInfo.setDescription(description);
+            personInfo.setRoleName(passwd);
+            personInfo.setUpdateDate(new Date());
+            personInfo.setUpdatePerson(userInfo.getUserName());
+            userInfoService.updateUserInfo(personInfo);
+            logger.info("修改用户实例对象成功!");
+            return new ErrorMsg(Error.SUCCESS, "success", "");
+        } catch (Exception e) {
+            logger.error("{}", e);
+            return new ErrorMsg(Error.API_RESPONSE_EXCEPTION, "faile", "");
+        }
+    }
+    
+    /**
+     * 用户注册
+     *
+     * @param userCode
+     * @param userName
+     * @param description
+     * @param passwd
+     * @param roleName
+     * @param organizeName
+     * @param session
+     * @return
+     */
+    @RequestMapping("/info/addpersoninfo.do")
+    @ResponseBody
+    public  ErrorMsg addPersonInfo(@RequestParam(value = "userCode", required = false)String userCode, 
+    		@RequestParam(value = "userName", required = false)String userName, 
+    		@RequestParam(value = "description", required = false)String description, 
+    		@RequestParam(value = "passwd", required = false)String passwd, 
+    		HttpServletRequest request,HttpSession session) {
+        try {
+        	UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
+            if (null == userInfo || null == userInfo.getUserName()) {
+                logger.error("用户Session过期");
+                return new ErrorMsg(Error.SESSION_EXPIRED_EXCEPTION, "Session expired");
+            }
+            UserInfo personInfo = new UserInfo();
+            personInfo.setId(UUIDKeyGenerator.getUUID());
+            personInfo.setUserCode(userCode);
+            personInfo.setUserName(userName);
+            //对密码进行MD5加密
+            Md5PasswordEncoder encoder = new Md5PasswordEncoder();
+            String encoded = encoder.encodePassword(passwd, userName);
+            personInfo.setPassword(encoded);
+            personInfo.setDescription(description);
+            personInfo.setCreateDate(new Date());
+            personInfo.setCreatePerson(userInfo.getUserName());
+            userInfoService.addUserInfo(personInfo);
+            logger.info("添加新用户实例对象成功!");
+            return new ErrorMsg(Error.SUCCESS, "success", "");
+        } catch (Exception e) {
+            logger.error("{}", e);
+            return new ErrorMsg(Error.API_RESPONSE_EXCEPTION, "faile", "");
+        }
     }
 
     @RequestMapping(value = "/info/delete.do")
