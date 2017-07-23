5390095984628ddf853b2de984ebdfbfa4a59ee5
Barrie <barrieshieh@gmail.com>
2017-07-24 01:25:38 +0800

    添加t031

--- src/com/elefirst/base/utils/DateUtil.java
+++ src/com/elefirst/base/utils/DateUtil.java
@@ -1,27 +1,59 @@
 package com.elefirst.base.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateUtil {
-	/** 
+    /**
      * 将String型格式化,比如想要将2011-11-11格式化成2011年11月11日,就StringPattern("2011-11-11","yyyy-MM-dd","yyyy年MM月dd日").
-     * @param date String 想要格式化的日期 "yyyy年MM月dd日 HH时mm分ss秒SSS毫秒"
+     *
+     * @param date       String 想要格式化的日期 "yyyy年MM月dd日 HH时mm分ss秒SSS毫秒"
      * @param oldPattern String 想要格式化的日期的现有格式
      * @param newPattern String 想要格式化成什么格式
-     * @return String 
-     */ 
-    public static String StringPattern(String date, String oldPattern, String newPattern) { 
-        if (date == null || oldPattern == null || newPattern == null) 
-            return ""; 
-        SimpleDateFormat sdf1 = new SimpleDateFormat(oldPattern) ;        // 实例化模板对象  
-        SimpleDateFormat sdf2 = new SimpleDateFormat(newPattern) ;        // 实例化模板对象  
-        Date d = null ;  
-        try{  
-            d = sdf1.parse(date) ;   // 将给定的字符串中的日期提取出来  
-        }catch(Exception e){            // 如果提供的字符串格式有错误，则进行异常处理  
-            e.printStackTrace() ;       // 打印异常信息  
-        }  
+     * @return String
+     */
+    public static String StringPattern(String date, String oldPattern, String newPattern) {
+        if (date == null || oldPattern == null || newPattern == null)
+            return "";
+        SimpleDateFormat sdf1 = new SimpleDateFormat(oldPattern);        // 实例化模板对象
+        SimpleDateFormat sdf2 = new SimpleDateFormat(newPattern);        // 实例化模板对象
+        Date d = null;
+        try {
+            d = sdf1.parse(date);   // 将给定的字符串中的日期提取出来
+        } catch (Exception e) {            // 如果提供的字符串格式有错误，则进行异常处理
+            e.printStackTrace();       // 打印异常信息
+        }
         return sdf2.format(d);
-    } 
+    }
+
+    /**
+     * 获取两个日期内所有日期
+     *
+     * @param from 开始日期
+     * @param to   截止日期
+     * @return 两个日期之间的所有日期
+     */
+    public static String[] getAllDays(String from, String to) {
+        String[] days = null;
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+            long toDay = format.parse(to).getTime();
+            long fromDay = format.parse(from).getTime();
+            long i = (toDay - fromDay) / (1000 * 60 * 60 * 24);
+            GregorianCalendar calendar = new GregorianCalendar();
+            calendar.setTime(format.parse(from));
+            days = new String[Integer.valueOf(String.valueOf(i)) + 1];
+            days[0] = from;
+            for (int j = 1; j <= i; j++) {
+                calendar.add(Calendar.DATE, 1);
+                days[j] = format.format(calendar.getTime());
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return days;
+    }
 }
--- src/com/elefirst/report/controller/ReportT031Controller.java
+++ src/com/elefirst/report/controller/ReportT031Controller.java
@@ -3,8 +3,11 @@ package com.elefirst.report.controller;
 import com.elefirst.base.controller.BaseController;
 import com.elefirst.base.entity.Error;
 import com.elefirst.base.entity.ErrorMsg;
+import com.elefirst.base.utils.DateUtil;
 import com.elefirst.power.po.DataT031;
 import com.elefirst.power.service.iface.IDataT031Service;
+import com.elefirst.system.po.PnInfo;
+import com.elefirst.system.service.iface.IPnInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by barrie on 2017/7/23.
@@ -26,6 +33,9 @@ import java.util.List;
 public class ReportT031Controller extends BaseController {
     @Autowired
     private IDataT031Service dataT031Service;
+
+    @Autowired
+    private IPnInfoService pnInfoService;
 
     @RequestMapping(value = "/daily/list.do")
     @ApiOperation(value = "列表", notes = "", httpMethod = "POST")
@@ -46,10 +56,59 @@ public class ReportT031Controller extends BaseController {
             template.setPage(page);
             template.setRows(rows);
         }
+        PnInfo pnInfoTpl = new PnInfo();
+        if (null != areaId) {
+            pnInfoTpl.setAreaId(areaId);
+        }
 
-        List<DataT031> result = dataT031Service.getDataT031List(template, startTime, endTime, hour, minute);
+        List<PnInfo> pnInfos = pnInfoService.getPnInfoList(pnInfoTpl);
+
+        List<DataT031> dataT031s = dataT031Service.getDataT031List(template, startTime, endTime, hour, minute);
+
+        String[] days = DateUtil.getAllDays(startTime, endTime);
+
+        List<Map<String, String>> report = new ArrayList<>();
+
+        for (int i = 0; i < pnInfos.size(); i++) {
+            PnInfo pnInfo = pnInfos.get(i);
+            Map<String, String> item = new LinkedHashMap<>();
+            item.put("监测点", pnInfo.getName());
+
+            for (int j = 0; j < days.length; j++) {
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+                SimpleDateFormat oformat = new SimpleDateFormat("MM-dd");
+                String title = oformat.format(format.parse(days[j]));
+
+                DataT031 dataT031 = getDataT031(dataT031s, pnInfo.getAreaId(), pnInfo.getConcentratorId(), pnInfo.getPn(), days[j]);
+                item.put(title, dataT031.getTotalpositiveactivepower());
+            }
+            report.add(item);
+        }
 
 
-        return new ErrorMsg(Error.SUCCESS, "success", result);
+        return new ErrorMsg(Error.SUCCESS, "success", report);
+    }
+
+    private PnInfo getPnInfo(List<PnInfo> pnInfos, PnInfo pnInfo) {
+        for (int i = 0; i < pnInfos.size(); i++) {
+            if (pnInfos.get(i).getAreaId().equals(pnInfo.getAreaId()) && pnInfos.get(i).getConcentratorId().equals(pnInfo.getConcentratorId()) && pnInfos.get(i).getPn().equals(pnInfo.getPn())) {
+                return pnInfos.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    private DataT031 getDataT031(List<DataT031> list, String areaId, String concentratorId, String pn, String date) {
+        for (int i = 0; i < list.size(); i++) {
+            String itemTime = list.get(i).getClientoperationtime();
+            String itemAreaId = list.get(i).getAreaId();
+            String itemConcentratorId = list.get(i).getConcentratorId();
+            String itemPn = list.get(i).getPn();
+            if (itemTime.substring(0, 8).equals(date.substring(0, 8)) && itemAreaId.equals(areaId) && itemConcentratorId.equals(concentratorId) && itemPn.equals(pn)) {
+                return list.get(i);
+            }
+        }
+        return new DataT031();
     }
 }
