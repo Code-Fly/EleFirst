16c9055cb0578fde93746f4c652d04453610bda4
Barrie <barrieshieh@gmail.com>
2017-07-30 16:16:21 +0800

    添加抄见电量小时报表

---
+++ WebRoot/view/report/chaojiandianliang-daily/chaojiandianliang-daily.jsp
@@ -0,0 +1,43 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: barrie
+  Date: 17/1/26
+  Time: 上午2:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="/view/common/config.jsp" %>
+<html>
+<head>
+    <!-- 公共属性 -->
+    <%@ include file="/view/common/meta.jsp" %>
+    <%@ include file="/view/common/common960.jsp" %>
+    <script type="text/javascript">
+        require(["js/chaojiandianliang-daily.js"]);
+    </script>
+</head>
+<body>
+<table id="tt2" class="easyui-datagrid"
+       toolbar="#tb"
+       singleSelect="true" border="false" fit="true">
+</table>
+
+<div id="tb" style="padding:5px;height:auto">
+    <div>
+        <input id="datebox-time-start" label="开始时间" labelAlign="right" type="text" class="easyui-datebox"
+               style="width:220px">
+        <input id="datebox-time-end" label="结束时间" labelAlign="right" type="text" class="easyui-datebox"
+               style="width:220px">
+        <input id="numberspinner-hour" label="时" labelAlign="right" class="easyui-numberspinner"
+               style="width:150px;">
+        <input id="numberspinner-minute" label="分" labelAlign="right" class="easyui-numberspinner"
+               style="width:150px;">
+
+        <a href="#" id="btn-detail-search" class="easyui-linkbutton"
+           data-options="plain:true,iconCls:'icon-search'">查询</a>
+        <a href="#" id="btn-detail-export" class="easyui-linkbutton"
+           data-options="plain:true,iconCls:'icon-page_excel'">导出</a>
+    </div>
+</div>
+</body>
+</html>
---
+++ WebRoot/view/report/chaojiandianliang-daily/js/chaojiandianliang-daily.js
@@ -0,0 +1,177 @@
+$(document).ready(function () {
+    var _spinner = new Spinner();
+
+
+    $("#datebox-time-start").datebox("calendar").calendar({
+        firstDay: 1
+    });
+
+    $("#datebox-time-end").datebox("calendar").calendar({
+        firstDay: 1
+    });
+
+    $("#datebox-time-start").datebox({
+        required: true,
+        editable: false
+    });
+
+    $("#datebox-time-end").datebox({
+        required: true,
+        editable: false
+    });
+
+    $("#numberspinner-hour").numberspinner({
+        required: true,
+        editable: false,
+        value: 0,
+        min: 0,
+        max: 23,
+        increment: 1
+    });
+
+    $("#numberspinner-minute").numberspinner({
+        required: true,
+        editable: false,
+        value: 0,
+        min: 0,
+        max: 45,
+        increment: 15
+    });
+
+    $("#tt2").datagrid({
+        pagination: true,
+        rownumbers: true,
+        pageSize: DEFAULT_PAGE_SIZE,
+        pageList: DEFAULT_PAGE_LIST,
+        pageNumber: 1,
+        singleSelect: true,
+        fit: true,
+        border: false,
+        // url: _ctx + 'report/display/daily/list.do',
+        // queryParams: {
+        //     date: $("#startdate").datebox("getValue"),
+        //     areaId: _areaId
+        // },
+    });
+
+    $("#btn-detail-search").linkbutton({
+        onClick: function () {
+            if (!$("#datebox-time-start").datebox("isValid")) {
+                $.messager.alert("信息提示", "请选择开始时间！", "info");
+                return;
+            }
+
+            if (!$("#datebox-time-end").datebox("isValid")) {
+                $.messager.alert("信息提示", "请选择结束时间！", "info");
+                return;
+            }
+
+            loadData();
+        }
+    });
+
+    $("#btn-detail-export").linkbutton({
+        onClick: function () {
+            var startDate = TimeUtils.dataBoxDateToDate($("#datebox-time-start").datebox("getValue"));
+            var endDate = TimeUtils.dataBoxDateToDate($("#datebox-time-end").datebox("getValue"));
+            endDate.setTime(endDate.getTime() + (24 * 60 * 60 * 1000 - 1));
+
+            var hour = parseInt($("#numberspinner-hour").numberspinner("getValue"));
+            var minute = parseInt($("#numberspinner-minute").numberspinner("getValue"));
+
+            var param = $.param({
+                areaId: _areaId,
+                hour: fixNum(hour, 2),
+                minute: fixNum(minute, 2),
+                startTime: startDate.format("yyyyMMddhhmmss"),
+                endTime: endDate.format("yyyyMMddhhmmss")
+            });
+            window.location.href = _ctx + "report/t037/daily/export.do?" + param;
+        }
+    });
+
+    init();
+
+    function init() {
+        var endDate = new Date();
+
+        var startDate = new Date();
+        startDate.setDate(startDate.getDate() - 30);
+
+        $("#datebox-time-start").datebox("setValue", startDate.getFullYear() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getDate());
+
+        $("#datebox-time-end").datebox("setValue", endDate.getFullYear() + "-" + (endDate.getMonth() + 1) + "-" + endDate.getDate());
+
+        setTimeout(function () {
+            loadData();
+        }, 500);
+    }
+
+    function loadData() {
+        var startDate = TimeUtils.dataBoxDateToDate($("#datebox-time-start").datebox("getValue"));
+        var endDate = TimeUtils.dataBoxDateToDate($("#datebox-time-end").datebox("getValue"));
+        endDate.setTime(endDate.getTime() + (24 * 60 * 60 * 1000 - 1));
+
+        var hour = parseInt($("#numberspinner-hour").numberspinner("getValue"));
+        var minute = parseInt($("#numberspinner-minute").numberspinner("getValue"));
+
+        $.ajax({
+            url: _ctx + "report/t037/daily/list.do",
+            type: "POST",
+            cache: false,
+            data: {
+                areaId: _areaId,
+                hour: fixNum(hour, 2),
+                minute: fixNum(minute, 2),
+                startTime: startDate.format("yyyyMMddhhmmss"),
+                endTime: endDate.format("yyyyMMddhhmmss"),
+                page: 1,
+                rows: 1
+            },
+            success: function (r) {
+                if (r.hasOwnProperty("errcode")) {
+                    if ("0" == r.errcode) {
+                        var frozenColumns = [
+                            {field: '监测点', title: '监测点', width: 100, align: 'center'},
+                        ];
+
+                        $("#tt2").datagrid({
+                            url: _ctx + "report/t037/daily/list.do",
+                            queryParams: {
+                                areaId: _areaId,
+                                hour: fixNum(hour, 2),
+                                minute: fixNum(minute, 2),
+                                startTime: startDate.format("yyyyMMddhhmmss"),
+                                endTime: endDate.format("yyyyMMddhhmmss"),
+                            },
+                            frozenColumns: [frozenColumns],
+                            columns: [DataGridUtils.getColumn(r.data, frozenColumns)]
+                        });
+
+                    } else {
+                        jError("请求失败！" + ErrUtils.getMsg(r.errcode));
+                    }
+                } else {
+                    jError("请求失败！" + ErrUtils.getMsg("2"));
+                }
+            },
+            error: function (request) {
+                jError("请求失败！" + ErrUtils.getMsg("3"));
+            },
+            beforeSend: function (XMLHttpRequest) {
+                $("#tt2").datagrid("loadData", []);
+                _spinner.load();
+            },
+            complete: function (XMLHttpRequest, textStatus) {
+                _spinner.unload();
+            }
+        });
+    }
+
+
+    //刷新页面
+    function refreshClick() {
+        window.location.reload();
+    }
+});
+
